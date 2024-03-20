4f86e5d91113e3874ee50e5c600d3ca4088c8196
Simon Quigley <squigley@altispeed.com>
2024-03-20 21:59:20 +0000

    Add the appropriate backend components

--- src/backend/InvenTree/order/models.py
+++ src/backend/InvenTree/order/models.py
@@ -1000,7 +1000,7 @@ class SalesOrder(TotalPriceMixin, Order):
                 )
 
             # Only an open order can be marked as shipped
-            elif not self.is_open:
+            elif not self.is_open and not self.is_completed:
                 raise ValidationError(_('Only an open order can be marked as complete'))
 
             elif self.pending_shipment_count > 0:
@@ -1042,9 +1042,12 @@ class SalesOrder(TotalPriceMixin, Order):
         if not self.can_complete(**kwargs):
             return False
 
-        self.status = SalesOrderStatus.SHIPPED.value
-        self.shipped_by = user
-        self.shipment_date = InvenTree.helpers.current_date()
+        if self.status == SalesOrderStatus.SHIPPED:
+            self.status = SalesOrderStatus.COMPLETE.value
+        else:
+            self.status = SalesOrderStatus.SHIPPED.value
+            self.shipped_by = user
+            self.shipment_date = InvenTree.helpers.current_date()
 
         self.save()
 
